22b10b56bb910448194af915f191f6943d155aba
bitsydarel <bitsydarel@gmail.com>
2021-01-18 18:09:38 +0200

    Initial commit

---
+++ bin/dart_github_hooks.dart
@@ -0,0 +1,5 @@
+import 'package:dart_github_hooks/dart_github_hooks.dart' as dart_github_hooks;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_github_hooks.calculate()}!');
+}
---
+++ test/dart_github_hooks_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_github_hooks/dart_github_hooks.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
